6571fd312d6eb0866436c7397aad1fa44d25dbb4
catcatstudio <83664742+catcatstudio@users.noreply.github.com>
2021-12-23 17:54:43 +0800

    Synchronize

--- Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -45,39 +45,39 @@ namespace Complete
 
         private void Update ()
         {
-            // The slider should have a default value of the minimum launch force.
+            //飛彈最小默認值
             m_AimSlider.value = m_MinLaunchForce;
 
-            // If the max force has been exceeded and the shell hasn't yet been launched...
+            //如果已經超過最大力並且砲彈還沒有發射......
             if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
             {
-                // ... use the max force and launch the shell.
+                // 使用最大力量 並且發射
                 m_CurrentLaunchForce = m_MaxLaunchForce;
                 Fire ();
             }
-            // Otherwise, if the fire button has just started being pressed...
+            //如果按下按鈕的話.......
             else if (Input.GetButtonDown (m_FireButton))
             {
-                // ... reset the fired flag and reset the launch force.
+                //重置發射標誌並重置發射力
                 m_Fired = false;
                 m_CurrentLaunchForce = m_MinLaunchForce;
 
-                // Change the clip to the charging clip and start it playing.
+                //將clip更改為ChargingClip並且播放
                 m_ShootingAudio.clip = m_ChargingClip;
                 m_ShootingAudio.Play ();
             }
-            // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
+            //如果按住開火按鈕而外殼尚未啟動......
             else if (Input.GetButton (m_FireButton) && !m_Fired)
             {
-                // Increment the launch force and update the slider.
+                //增加發射力並更新滑塊
                 m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
                 m_AimSlider.value = m_CurrentLaunchForce;
             }
-            // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+            //如果釋放開火按鈕並且外殼尚未啟動......
             else if (Input.GetButtonUp (m_FireButton) && !m_Fired)
             {
-                // ... launch the shell.
+                //開火
                 Fire ();
             }
         }
@@ -85,21 +85,38 @@ namespace Complete
 
         private void Fire()
         {
+            //飛彈飛射
             m_Fired = true;
+            //創建新的飛彈 且套用鋼體
             Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            //傳"FirOther"函式給 RpcTarget.Others   => 傳送m_FireTransform.position
             photonView.RPC("FireOther", RpcTarget.Others, m_FireTransform.position);
+            //飛彈的力量*方向
             shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+            //聲音
             m_ShootingAudio.clip = m_FireClip;
+            //撥放
             m_ShootingAudio.Play();
+            //重置發射力。 這是在缺少按鈕事件的情況下的預防措施。
             m_CurrentLaunchForce = m_MinLaunchForce;
         }
+        //傳送資料
         [PunRPC]
-        private void FireOther(Vector3 pos)
+        private void FireOther(Vector3 pos) //設定FireOther函式
         {
+            //pos是Tank的位置
+
+            //飛彈飛射
             m_Fired = true;
+            //產生新的飛彈
             Rigidbody shellInstance = Instantiate(m_Shell, pos, m_FireTransform.rotation) as Rigidbody;
+            //飛彈的力量*方向
             shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+            //重置發射力。 這是在缺少按鈕事件的情況下的預防措施。
             m_CurrentLaunchForce = m_MinLaunchForce;
-        }
+
+            //Debug.Log("位置是:"+pos);
+        }         
+        
     }
 }
